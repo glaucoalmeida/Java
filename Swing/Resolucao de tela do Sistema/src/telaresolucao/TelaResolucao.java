1ebe687dff1421fe1fea5cfbf9baa996f471d013
Glauco Almeida <jglaucot@gmail.com>
2022-12-16 18:49:51 -0300

    Java

--- Swing/Resolucao de tela do Sistema/src/telaresolucao/TelaResolucao.java
+++ Swing/Resolucao de tela do Sistema/src/telaresolucao/TelaResolucao.java
@@ -36,12 +36,8 @@ public class TelaResolucao extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(255, 51, 51));
         jLabel1.setText("Resolução de Tela");
 
-        lblBot.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        lblBot.setForeground(new java.awt.Color(0, 0, 255));
         lblBot.setText("Clique no Botão");
 
         btnVer.setText("Verificar");
@@ -60,34 +56,26 @@ public class TelaResolucao extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel3)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(6, 6, 6)
-                                .addComponent(lblBot))))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(50, 50, 50)
-                        .addComponent(btnVer)))
-                .addContainerGap(15, Short.MAX_VALUE))
+                    .addComponent(lblBot)
+                    .addComponent(jLabel1)
+                    .addComponent(btnVer))
+                .addContainerGap(23, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel3)
-                        .addContainerGap(17, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel3)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(lblBot)
                         .addGap(18, 18, 18)
-                        .addComponent(btnVer)
-                        .addGap(11, 11, 11))))
+                        .addComponent(lblBot)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnVer)))
+                .addContainerGap(17, Short.MAX_VALUE))
         );
 
         pack();
