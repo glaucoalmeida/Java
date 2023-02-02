da5248bc4a5dc1b66af1bd417a36437d1333f8d2
Glauco Almeida <jglaucot@gmail.com>
2023-02-02 19:00:17 -0300

    Maven

--- Maven/Caneta/src/main/java/com/mycompany/programacaneta/ProgramaCaneta.java
+++ Maven/Caneta/src/main/java/com/mycompany/programacaneta/ProgramaCaneta.java
@@ -12,12 +12,8 @@ public class ProgramaCaneta {
 
     public static void main(String[] args) {
         Caneta c1 = new Caneta("BIC","Azul",1.5f,2,true);
-        c1.destampar();
-        c1.rabiscar();
         c1.status();
         Caneta c2 = new Caneta("Compacto","Preta",1.0f,1,false);
-        c2.tampar();
-        c2.rabiscar();
         c2.status();
     }
 }
--- Maven/Simulador de Banco/src/main/java/com/mycompany/programabanco/Conta.java
+++ Maven/Simulador de Banco/src/main/java/com/mycompany/programabanco/Conta.java
@@ -36,47 +36,47 @@ public class Conta {
     
     public void abrirConta(){
         this.setStatus(true);
-        if (this.getTipo() == "CC"){
+        if ("CC".equals(this.getTipo())){
             this.setSaldo(50);
-            System.out.println("Conta corrente aberta com sucesso. Você recebeu R$ 50,00");
-        } if (this.getTipo() == "CP"){
+            System.out.println("Conta corrente aberta com sucesso. Você recebeu R$ 50,00.");
+        } if ("CP".equals(this.getTipo())){
             this.setSaldo(150);
-            System.out.println("Conta poupança aberta com sucesso. Você recebeu R$ 150,00");
+            System.out.println("Conta poupança aberta com sucesso. Você recebeu R$ 150,00.");
         }
     }
     
     public void fecharConta(){
         if (this.getSaldo() < 0){
-            System.out.println("Necessário pagar o débito restante");
+            System.out.println("Impossível fechar a conta. Necessário pagar o débito restante.");
         } else if (this.getSaldo() > 0){
-            System.out.println("Necessário sacar o saldo restante");
+            System.out.println("Impossível fechar a conta. Necessário sacar o saldo restante.");
         } else {
             this.setStatus(false);
-            System.out.println("Conta fechada com sucesso");
+            System.out.println("Conta fechada com sucesso.");
         }
     }
     
     public void depositar(float d){
         this.setSaldo(this.getSaldo() + d);
-        System.out.println("Depósito realizado no valor de R$ " + String.format("%.2f", d));
+        System.out.println("Depósito realizado no valor de R$ " + String.format("%.2f", d) + ".");
     }
     
     public void sacar(float s){
         if (this.getSaldo() == s || this.getSaldo() > s){
             this.setSaldo(this.getSaldo() - s);
-            System.out.println("Saque realizado no valor de R$ " + String.format("%.2f", s));
+            System.out.println("Saque realizado no valor de R$ " + String.format("%.2f", s ) + ".");
         } else {
-            System.out.println("Valor de saque maior que o valor atual");
+            System.out.println("Valor de saque maior que o valor atual.");
         }
     }
     
     public void pagMensal(){
-        if (this.getTipo() == "CC"){
+        if ("CC".equals(this.getTipo())){
             this.setSaldo(this.getSaldo() - 12);
-            System.out.println("Pagamento mensal realizado no valor de R$ 12,00");
-        } if (this.getTipo() == "CP"){
+            System.out.println("Pagamento mensal realizado no valor de R$ 12,00.");
+        } if ("CP".equals(this.getTipo())){
             this.setSaldo(this.getSaldo() - 20);
-            System.out.println("Pagamento mensal realizado no valor de R$ 20,00");
+            System.out.println("Pagamento mensal realizado no valor de R$ 20,00.");
         }
     }
 
